be95f0232c2bc2d56f0c663d1afd7c4d61b4883b
nnam-ibe <nnam.ibe@gmail.com>
2018-11-03 23:46:13 -0400

    Adjusted NavBar, started User Menu

--- client/src/components/navigation/Navbar.jsx
+++ client/src/components/navigation/Navbar.jsx
@@ -3,6 +3,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import IconButton from '@material-ui/core/IconButton';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 import firebaseUtil from '../../utils/firebaseUtil.js';
 
 class Navbar extends Component {
@@ -14,22 +18,25 @@ class Navbar extends Component {
 		if (firebaseUtil.getCurrentUser()) buttonLabel = 'Logout';
 
 		this.state = {
-			buttonLabel: buttonLabel
+			buttonLabel: buttonLabel,
+			isLoggedIn: false,
+			userMenuAnchor: null,
+			userMenuOpen: false
 		};
 	}
 
 	componentDidMount() {
 		firebaseUtil.onAuthStateChanged((user) => {
 			if (user) {
-				this.setState({ buttonLabel: 'Logout' });
+				this.setLoggedInState();
 			} else {
-				this.setState({ buttonLabel: 'Login' });
+				this.setLoggedOutState();
 			}
-		})
+		});
 	}
 
 	onClickLogin = () => {
-		if (firebaseUtil.getCurrentUser()) {
+		if (this.state.isLoggedIn) {
 			firebaseUtil.logout().then(() => {
 				this.setState({ buttonLabel: 'Login' });
 				this.props.history.push('/login');
@@ -46,15 +53,73 @@ class Navbar extends Component {
 			<div className='Navbar'>
 				<AppBar position='fixed' color='default'>
 					<Toolbar>
-						<Typography variant='title' color='inherit'>
-							<a href='/' className='no-a-style'>Title</a>
-						</Typography>
-						<Button color='inherit' className='pull-right' onClick={this.onClickLogin}>{buttonLabel}</Button>
+						<div className='display-flex width-100'>
+							<Typography variant='h6' color='inherit' className='flex-grow-1 text-align-left'>
+								<a href='/' className='no-a-style'>Title</a>
+							</Typography>
+							<div>
+								{
+									this.state.isLoggedIn && (
+										<IconButton onClick={this._openUserMenu}>
+											<AccountCircle />
+											<UserMenu
+												anchorEl={this.state.userMenuAnchor}
+												open={this.state.userMenuOpen}
+												onClose={this._closeUserMenu}
+											/>
+										</IconButton>
+									)
+								}
+								<Button color='inherit' className='display-flex' onClick={this.onClickLogin}>{buttonLabel}</Button>
+							</div>
+						</div>
 					</Toolbar>
 				</AppBar>
 			</div>
 		);
 	}
+
+	setLoggedInState = () => this.setState({ buttonLabel: 'Logout', isLoggedIn: true });
+
+	setLoggedOutState = () => this.setState({ buttonLabel: 'Login', isLoggedIn: false });
+
+	_openUserMenu = (event) => {
+		this.setState({
+			userMenuAnchor: event.currentTarget,
+			userMenuOpen: true
+		})
+	}
+
+	// State is not being set here for some reason
+	_closeUserMenu = () => {
+		this.setState({
+			userMenuAnchor: null,
+			userMenuOpen: false
+		});
+	}
 }
 
 export default Navbar;
+
+function UserMenu (props) {
+	return (
+		<Menu
+			id="menu-appbar"
+			anchorEl={props.anchorEl}
+			anchorOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			transformOrigin={{
+				vertical: 'top',
+				horizontal: 'right',
+			}}
+			open={props.open}
+			onClose={props.onClose}
+		>
+			<MenuItem onClick={props.onClose}>Profile</MenuItem>
+			<MenuItem onClick={props.onClose}>My account</MenuItem>
+		</Menu>
+
+	);
+}
