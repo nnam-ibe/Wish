cd31cdae6116e99d74627df5b3eac40f8f2cde5b
Nnamdi Ibe <nnam.ibe@gmail.com>
2019-08-23 17:13:36 -0400

    Added time out to allow for component to unmount

--- client/src/components/app/App.test.jsx
+++ client/src/components/app/App.test.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import App from './app.jsx';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<BrowserRouter><Route path='/' component={App}/></BrowserRouter>, div);
-  ReactDOM.unmountComponentAtNode(div);
+	const div = document.createElement('div');
+	ReactDOM.render(<BrowserRouter><Route path='/' component={App}/></BrowserRouter>, div);
+	// Timeout to allow firebase to unsubscribe
+	setTimeout(() => ReactDOM.unmountComponentAtNode(div), 100);
 });
