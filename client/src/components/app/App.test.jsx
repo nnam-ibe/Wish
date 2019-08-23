2e7ed384ad426ec7ff9b08278bca1f3751eb49cc
Nnamdi Ibe <nnam.ibe@gmail.com>
2019-08-23 12:50:51 -0400

    Updated app test file

--- client/src/components/app/App.test.jsx
+++ client/src/components/app/App.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter, Route } from 'react-router-dom';
+import App from './app.jsx';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<BrowserRouter><Route path='/' component={App}/></BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
