f717ed343a81d801ffe75b51be0f7902f16c7a42
Nnamdi Ibe <nnam.ibe@gmail.com>
2019-10-17 23:30:58 -0400

    moved default field values to file

---
+++ client/src/utils/FormFieldDefaults.js
@@ -0,0 +1,40 @@
+exports.addTaxesDefault = {
+	checked: false,
+	value: 'addTaxes'
+};
+
+exports.emailDefault = {
+	error: false,
+	helperText: '',
+	value: ''
+};
+
+exports.incrementDefault = {
+	error: false,
+	helperText: '',
+	value: '200'
+};
+
+exports.nameDefault = {
+	error: false,
+	helperText: '',
+	value: ''
+};
+
+exports.passwordDefault = {
+	error: false,
+	helperText: '',
+	value: ''
+};
+
+exports.priceDefault = {
+	error: false,
+	helperText: '',
+	value: ''
+};
+
+exports.savedDefault = {
+	error: false,
+	helperText: '',
+	value: ''
+};
