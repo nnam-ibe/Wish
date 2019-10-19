1782f576b810839b549d5c6af1aba4f172addee0
Nnamdi Ibe <nnam.ibe@gmail.com>
2019-10-19 00:02:12 -0400

    Changed to react hook

--- client/src/components/auth/CreateAccount.jsx
+++ client/src/components/auth/CreateAccount.jsx
@@ -1,183 +1,199 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import _ from 'lodash';
-import FetchUtil from '../../utils/fetchUtil.js';
-import InputValidation from '../../utils/InputValidation.js';
+import FetchUtil from '../../utils/fetchUtil';
+import FirebaseUtil from '../../utils/firebaseUtil';
+import InputValidation from '../../utils/InputValidation';
+import { emailDefault, passwordDefault, usernameDefault } from '../../utils/FormFieldDefaults';
 
-const validField = { error: false, helperText: '' };
+function CreateAccount(props) {
+	const [username, setUsername] = useState(usernameDefault);
+	const [email, setEmail] = useState(emailDefault);
+	const [password, setPassword] = useState(passwordDefault);
+	const [confirmPassword, setConfirmPassword] = useState(passwordDefault);
+	const [showProgressBar, setShowProgressBar] = useState(false);
+	const progessBar = <LinearProgress/>;
 
-class CreateAccount extends Component {
-
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			fields: {
-				username: validField,
-				email: validField,
-				password: validField,
-				confirmPassword: validField
-			},
-			showProgressBar: false
-		};
-	};
-
-	createAccount = (e) => {
+	function handleCreateClick(e) {
 		e.preventDefault();
-		this.setState({ showProgressBar: true });
-		let args = {
-			username: document.getElementById('username'),
-			email: document.getElementById('email'),
-			password: document.getElementById('password'),
-			confirmPassword: document.getElementById('confirmPassword')
-		};
+		setShowProgressBar(true);
 
-		if ( !this._validateInputs(args) ) {
-			this.setState({ showProgressBar: false });
-			return;
-		}
+		if (validateInputs()) return setShowProgressBar(false);
 
 		FetchUtil.put(`/api/create/account`, {
-			username: _.trim(args.username.value),
-			email: args.email.value,
-			password: args.password.value
+			username: _.trim(username.value),
+			email: _.trim(email.value),
+			password: password.value
 		})
 		.then(res => {
+			setShowProgressBar(false);
 			if (res.ok) {
-				return this.props.history.push('/login');
+				return props.history.push('/login');
 			}
 			return res.json();
 		})
-		.then(data => {
-			let fields = this.state.fields;
-			if (_.includes(data.code, 'email')) {
-				_.set(fields, 'email', { error: true, helperText: data.message });
+		.then(err => {
+			const message = FirebaseUtil.getErrorMessage(err.code);
+			if (_.includes(err.code, 'password')) {
+				setPassword({ ...password, error: true, helperText: message });
 			} else {
-				_.set(fields, 'password', { error: true, helperText: data.message });
-				_.set(fields, 'confirmPassword', { error: true, helperText: data.message });
+				setEmail({ ...email, error: true, helperText: message })
 			}
-			this.setState({ fields, showProgressBar: false });
 		})
 		.catch(err => {
-			let fields = this.state.fields;
-			_.set(fields, 'email', { error: true, helperText: err.toString() });
-			this.setState({ fields, showProgressBar: false });
+			setEmail({ ...email, error: true, helperText: err.toString() });
 		});
-	};
-
-	render() {
-		let fields = this.state.fields;
-		let progessBar = ( <LinearProgress/> );
-
-		return (
-			<div className='auth-component'>
-				<Paper>
-					{ this.state.showProgressBar && progessBar }
-					<div className='auth-paper'>
-						<form>
-							<Typography variant='h6'>Create your Account</Typography>
-							<TextField
-								margin='dense'
-								id='username'
-								label='Username'
-								error={fields.username.error}
-								helperText={fields.username.helperText}
-								fullWidth
-								required
-							/>
-							<TextField
-								margin='dense'
-								id='email'
-								label='Email Address'
-								type='email'
-								error={fields.email.error}
-								helperText={fields.email.helperText}
-								fullWidth
-								required
-							/>
-							<TextField
-								margin='dense'
-								id='password'
-								label='Password'
-								type='password'
-								error={fields.password.error}
-								helperText={fields.password.helperText}
-								fullWidth
-								required
-							/>
-							<TextField
-								margin='dense'
-								id='confirmPassword'
-								label='Confirm Password'
-								type='password'
-								error={fields.confirmPassword.error}
-								helperText={fields.confirmPassword.helperText}
-								fullWidth
-								required
-							/>
-							<Button
-								fullWidth
-								type='submit'
-								color='inherit'
-								onClick={this.createAccount}>
-								Create Account
-							</Button>
-						</form>
-					</div>
-				</Paper>
-			</div>
-		);
 	}
 
-	_validateInputs = (args) => {
-		const currentFields = this.state.fields, errors = {};
-		let err = InputValidation.validateUsername(args.username.value);
+	function validateInputs() {
+		let isInvalid = false;
+		let err = InputValidation.validateUsername(username.value);
 		if (err) {
-			errors.username = {
+			isInvalid = true;
+			setUsername({
+				...username,
 				error: true,
 				helperText: err.message
-			};
+			});
+		} else if (username.error) {
+			setUsername({
+				...username,
+				error: false,
+				helperText: ''
+			});
 		}
 
-		err = InputValidation.validateEmail(args.email.value);
+		err = InputValidation.validateEmail(email.value);
 		if (err) {
-			errors.email = {
+			isInvalid = true;
+			setEmail({
+				...email,
 				error: true,
 				helperText: err.message
-			};
+			});
+		} else if (email.error) {
+			setEmail({
+				...email,
+				error: false,
+				helperText: ''
+			});
 		}
 
-		err = InputValidation.validatePasswordsAreEqual(args.password.value, args.confirmPassword.value);
+		err = InputValidation.validatePasswordsAreEqual(password.value, confirmPassword.value);
 		if (err) {
-			errors.confirmPassword = errors.password = {
+			isInvalid = true;
+			setPassword({
+				...password,
 				error: true,
 				helperText: err.message
-			};
-		}
-
-		err = InputValidation.validatePassword(args.password.value);
-		if (err) {
-			errors.password = {
+			});
+			setConfirmPassword({
+				...confirmPassword,
 				error: true,
 				helperText: err.message
-			};
+			});
+		} else if (confirmPassword.error) {
+			setPassword({
+				...password,
+				error: false,
+				helperText: ''
+			});
+			setConfirmPassword({
+				...confirmPassword,
+				error: false,
+				helperText: ''
+			});
 		}
 
-		const newFieldsState = _.reduce(currentFields, (acc, fieldValue, fieldKey) => {
-				if (errors[fieldKey]) {
-					acc[fieldKey] = errors[fieldKey];
-				} else {
-					acc[fieldKey] = validField;
-				}
-				return acc;
-			}, {});
-		this.setState({ fields: newFieldsState });
-		return _.isEmpty(errors);
+		err = InputValidation.validatePassword(password.value);
+		if (err) {
+			isInvalid = true;
+			setPassword({
+				...password,
+				error: true,
+				helperText: err.message
+			});
+		} else if (password.error) {
+			setPassword({
+				...password,
+				error: false,
+				helperText: ''
+			});
+		}
+
+		return isInvalid;
 	}
-};
+
+	return (
+		<div className='auth-component'>
+			<Paper>
+				{ showProgressBar && progessBar }
+				<div className='auth-paper'>
+					<form>
+						<Typography variant='h6'>Create your Account</Typography>
+						<TextField
+							margin='dense'
+							id='username'
+							label='Username'
+							error={username.error}
+							value={username.value}
+							helperText={username.helperText}
+							onChange={(e) => setUsername({...username, value: e.target.value})}
+							fullWidth
+							required
+						/>
+						<TextField
+							margin='dense'
+							id='email'
+							label='Email Address'
+							type='email'
+							error={email.error}
+							value={email.value}
+							helperText={email.helperText}
+							onChange={(e) => setEmail({...email, value: e.target.value})}
+							fullWidth
+							required
+						/>
+						<TextField
+							margin='dense'
+							id='password'
+							label='Password'
+							type='password'
+							error={password.error}
+							value={password.value}
+							helperText={password.helperText}
+							onChange={(e) => setPassword({...password, value: e.target.value})}
+							fullWidth
+							required
+						/>
+						<TextField
+							margin='dense'
+							id='confirmPassword'
+							label='Confirm Password'
+							type='password'
+							error={confirmPassword.error}
+							value={confirmPassword.value}
+							helperText={confirmPassword.helperText}
+							onChange={(e) => setConfirmPassword({...confirmPassword, value: e.target.value})}
+							fullWidth
+							required
+						/>
+						<Button
+							fullWidth
+							type='submit'
+							color='inherit'
+							onClick={handleCreateClick}>
+							Create Account
+						</Button>
+					</form>
+				</div>
+			</Paper>
+		</div>
+	);
+}
 
 export default CreateAccount;
