db036168039003fa9573ec256a4fbfeeccd05f43
Nnamdi Ibe <nnam.ibe@gmail.com>
2019-10-22 18:59:29 -0400

    changed testing library

--- client/src/components/auth/CreateAccount.test.js
+++ client/src/components/auth/CreateAccount.test.js
@@ -1,12 +1,8 @@
-import { act } from 'react-dom/test-utils';
 import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, { mount, shallow } from 'enzyme';
+import {render, fireEvent, act} from '@testing-library/react'
 
 import CreateAccount from './CreateAccount.jsx';
 import FetchWrapper from '../../utils/FetchWrapper.js';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 // mock api calls
 let resolve, reject;
@@ -17,77 +13,45 @@ FetchWrapper.put = jest.fn(() => new Promise((_resolve, _reject) => {
 
 describe('CreateAccount', () => {
 	it('CreateAccount appears', () => {
-		const component = shallow(<CreateAccount />);
-		expect(component.find('.create-account-form')).toHaveLength(1);
+		const { getByText } = render(<CreateAccount />);
+		expect(getByText('Create your Account'));
 	});
 
 	it('errors show up when fields are empty', () => {
-		const component = mount(<CreateAccount />);
-		expect(component.find('#username-helper-text')).toHaveLength(0);
-		expect(component.find('#email-helper-text')).toHaveLength(0);
-		expect(component.find('#password-helper-text')).toHaveLength(0);
-		component.find('#create-account-form-button')
-			.hostNodes()
-			.simulate('click', {
-				preventDefault() {}
-			});
+		const { getByText, getByRole } = render(<CreateAccount />);
 
-		const usernameError = 'Username must be between 1 & 16 characters';
-		const emailError = 'Email cannot be empty';
-		const passwordError = 'Password must have at least 6 characters';
-		expect(component.find('#username-helper-text').hostNodes().text()).toBe(usernameError);
-		expect(component.find('#email-helper-text').hostNodes().text()).toBe(emailError);
-		expect(component.find('#password-helper-text').hostNodes().text()).toBe(passwordError);
+		fireEvent.click(getByRole('button'));
+		expect(getByText('Username must be between 1 & 16 characters'));
+		expect(getByText('Email cannot be empty'));
+		expect(getByText('Password must have at least 6 characters'));
 	});
 
 	it('errors show up when passwords dont match', async () => {
 		const usernameValue = 'johnny';
-		const emailValue = 'johnny';
+		const emailValue = 'sample@mail.ca';
 		const passwordValue = '654321';
 		const confirmPasswordValue = 'different';
-		const component = mount(<CreateAccount />);
+		const { getByText, getByRole, container } = render(<CreateAccount />);
 
-		component.find('#username')
-			.hostNodes()
-			.simulate('change', { target: { value: usernameValue } });
-		component.find('#email')
-			.hostNodes()
-			.simulate('change', { target: { value: emailValue } });
-		component.find('#password')
-			.hostNodes()
-			.simulate('change', { target: { value: passwordValue } });
-		component.find('#confirmPassword')
-			.hostNodes()
-			.simulate('change', { target: { value: confirmPasswordValue } });
-		component.find('#create-account-form-button')
-			.hostNodes()
-			.simulate('click', { preventDefault() {} });
-
-		const passwordError = 'Passwords don\'t match';
-		expect(component.find('#confirmPassword-helper-text').hostNodes().text()).toBe(passwordError);
+		fireEvent.change(container.querySelector('#username'), { target: { value: usernameValue } });
+		fireEvent.change(container.querySelector('#email'), { target: { value: emailValue } });
+		fireEvent.change(container.querySelector('#password'), { target: { value: passwordValue } });
+		fireEvent.change(container.querySelector('#confirmPassword'), { target: { value: confirmPasswordValue } });
+		fireEvent.click(getByRole('button'));
+		expect(getByText('Passwords don\'t match'));
 	});
 
 	it('makes api call when it passes validation', async () => {
 		const usernameValue = 'johnny';
-		const emailValue = 'johnny';
+		const emailValue = 'sample@mail.ca';
 		const passwordValue = '654321';
-		const component = mount(<CreateAccount {...{history: []}}/>);
+		const { getByText, getByRole, container } = render(<CreateAccount {...{history: []}}/>);
 
-		component.find('#username')
-			.hostNodes()
-			.simulate('change', { target: { value: usernameValue } });
-		component.find('#email')
-			.hostNodes()
-			.simulate('change', { target: { value: emailValue } });
-		component.find('#password')
-			.hostNodes()
-			.simulate('change', { target: { value: passwordValue } });
-		component.find('#confirmPassword')
-			.hostNodes()
-			.simulate('change', { target: { value: passwordValue } });
-		component.find('#create-account-form-button')
-			.hostNodes()
-			.simulate('click', { preventDefault() {} });
+		fireEvent.change(container.querySelector('#username'), { target: { value: usernameValue } });
+		fireEvent.change(container.querySelector('#email'), { target: { value: emailValue } });
+		fireEvent.change(container.querySelector('#password'), { target: { value: passwordValue } });
+		fireEvent.change(container.querySelector('#confirmPassword'), { target: { value: passwordValue } });
+		fireEvent.click(getByRole('button'));
 
 		await act(async () => {
 			resolve({
@@ -98,30 +62,19 @@ describe('CreateAccount', () => {
 		expect(FetchWrapper.put.mock.calls[0][1].username).toBe(usernameValue);
 		expect(FetchWrapper.put.mock.calls[0][1].email).toBe(emailValue);
 		expect(FetchWrapper.put.mock.calls[0][1].password).toBe(passwordValue);
-		expect(component.prop('history')[0]).toBe('/login');;
 	});
 
 	it('displays validation error message from api', async () => {
 		const usernameValue = 'neverjohnny';
-		const emailValue = 'happensjohnny';
+		const emailValue = 'happens@johnny.ca';
 		const passwordValue = 'tools654321';
-		const component = mount(<CreateAccount />);
+		const { getByText, getByRole, container } = render(<CreateAccount />);
 
-		component.find('#username')
-			.hostNodes()
-			.simulate('change', { target: { value: usernameValue } });
-		component.find('#email')
-			.hostNodes()
-			.simulate('change', { target: { value: emailValue } });
-		component.find('#password')
-			.hostNodes()
-			.simulate('change', { target: { value: passwordValue } });
-		component.find('#confirmPassword')
-			.hostNodes()
-			.simulate('change', { target: { value: passwordValue } });
-		component.find('#create-account-form-button')
-			.hostNodes()
-			.simulate('click', { preventDefault() {} });
+		fireEvent.change(container.querySelector('#username'), { target: { value: usernameValue } });
+		fireEvent.change(container.querySelector('#email'), { target: { value: emailValue } });
+		fireEvent.change(container.querySelector('#password'), { target: { value: passwordValue } });
+		fireEvent.change(container.querySelector('#confirmPassword'), { target: { value: passwordValue } });
+		fireEvent.click(getByRole('button'));
 
 		await act(async () => {
 			resolve({
@@ -130,46 +83,31 @@ describe('CreateAccount', () => {
 				}
 			});
 		});
-		component.update();
 
 		expect(FetchWrapper.put.mock.calls[1][1].username).toBe(usernameValue);
 		expect(FetchWrapper.put.mock.calls[1][1].email).toBe(emailValue);
 		expect(FetchWrapper.put.mock.calls[1][1].password).toBe(passwordValue);
-
-		const errorMessage = 'Email is already in use';
-		expect(component.find('#email-helper-text').hostNodes().text()).toBe(errorMessage);
+		expect(getByText('Email is already in use'));
 	});
 
 	it('displays promise failure reason', async () => {
 		const usernameValue = 'neverjohnny';
-		const emailValue = 'happensjohnny';
+		const emailValue = 'happens@johnny.ca';
 		const passwordValue = 'tools654321';
-		const component = mount(<CreateAccount />);
+		const { getByText, getByRole, container } = render(<CreateAccount />);
 
-		component.find('#username')
-			.hostNodes()
-			.simulate('change', { target: { value: usernameValue } });
-		component.find('#email')
-			.hostNodes()
-			.simulate('change', { target: { value: emailValue } });
-		component.find('#password')
-			.hostNodes()
-			.simulate('change', { target: { value: passwordValue } });
-		component.find('#confirmPassword')
-			.hostNodes()
-			.simulate('change', { target: { value: passwordValue } });
-		component.find('#create-account-form-button')
-			.hostNodes()
-			.simulate('click', { preventDefault() {} });
+		fireEvent.change(container.querySelector('#username'), { target: { value: usernameValue } });
+		fireEvent.change(container.querySelector('#email'), { target: { value: emailValue } });
+		fireEvent.change(container.querySelector('#password'), { target: { value: passwordValue } });
+		fireEvent.change(container.querySelector('#confirmPassword'), { target: { value: passwordValue } });
+		fireEvent.click(getByRole('button'));
 
 		const errorMessage = 'Sample error message';
 		await act(async () => reject(errorMessage));
-		component.update();
 
 		expect(FetchWrapper.put.mock.calls[2][1].username).toBe(usernameValue);
 		expect(FetchWrapper.put.mock.calls[2][1].email).toBe(emailValue);
 		expect(FetchWrapper.put.mock.calls[2][1].password).toBe(passwordValue);
-
-		expect(component.find('#email-helper-text').hostNodes().text()).toBe(errorMessage);
+		expect(getByText('Sample error message'));
 	});
 });
